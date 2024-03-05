ffde2b2bc118d57b751d2c11507ebb4eac582229
abdullokhpro <abdulloxrasulov441@gmail.com>
2024-03-05 16:31:37 +0500

    inital commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+let navBar = document.getElementById ("header__nav-buttons__menu");
+let navList = document.getElementById ("header__nav__list");
+let backtop = document.getElementById ("backtop");
+let body = document.querySelector("body");
+let darkMode = document.querySelector(".dark-mode");
+let navshrink = document.getElementById ("navshrink")
+
+darkMode.addEventListener("click", function(){
+    body.classList.toggle("dark");
+})
+window.addEventListener("scroll", function() {
+    showBacktop();
+    showNavShrink();
+})
+
+
+navBar.addEventListener("click", function() {
+    navList.classList.toggle("show-nav__list")
+})
+
+function showBacktop(){
+    if (scrollY>200){
+        backtop.classList.add("show-backtop")
+    }
+
+    else {
+        backtop.classList.remove("show-backtop")
+    }
+} 
+
+function showNavShrink(){
+    if (scrollY>0){
+        navshrink.classList.add("show-navshrink")
+    }
+
+    else {
+        navshrink.classList.remove("show-navshrink")
+    }
+} 
